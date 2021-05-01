5c101e5efe41901fd3622ccebb8f71dc373447bc
Sabhanayagam Baskaran <sabhanayagambaskaran@SabhanaagamsMBP.lan1>
2021-05-01 16:22:59 -0500

    Sabha | Refactoring the create Art Lambda to support other operations like delete, update, query.

---
+++ Steps.md
@@ -0,0 +1,7 @@
+## Step 1
+
+Testing arts create lambda.
+
+```
+serverless invoke local --function create --path mocks/create-event.json
+```
--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -2,10 +2,10 @@ export default function handle(lambda) {
   return async function (event, context) {
     let body, statusCode;
     try {
-      body = await lambda(event.context);
+      body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
-      boody = { error: e.message };
+      body = { error: e.message };
       statusCode = 500;
     }
     return {
